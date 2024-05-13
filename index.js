2494cb8b6ba2063063d6ba59c248a764831e3e66
Eduardo Arciniega <jesuseduardoarciniegamoreno@gmail.com>
2024-05-13 01:20:41 -0600

    servidor node configurado para archivos estaticos

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
 
 const PORT = 4000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  // Manejando la solicitud de la ruta raíz
-  if (req.url === '/' || req.url === '/index.html') {
-    // Lee el archivo index.html
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-      if (err) {
-        // Si hay un error, envía una respuesta de error
-        res.writeHead(500);
-        res.end('Error interno del servidor');
-      } else {
-        // Si se lee correctamente, envía el contenido del archivo como respuesta
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  } else {
-    // Si la ruta no es la raíz, devuelve un error 404
-    res.writeHead(404);
-    res.end('Página no encontrada');
-  }
-});
+// Ruta estática para servir archivos HTML desde el directorio 'public'
+app.use(express.static(path.join(__dirname, './')));
 
-// Escuchando en el puerto 4000
-server.listen(PORT, () => {
+// Iniciar el servidor
+app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
