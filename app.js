44473afba76cb28e84c771895750ecc1130d84bf
Eduardo Arciniega <jesuseduardoarciniegamoreno@gmail.com>
2024-06-01 22:45:56 -0600

    Rediseño del index, y header

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const { Server } = require('socket.io');
+const { createServer } = require('http');
 
-// Configurar el puerto
-const port = process.env.PORT || 3000; // Puedes cambiar el puerto si es necesario
+const port = process.env.PORT || 3000; 
 
-// Configurar EJS como motor de plantillas
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Configurar Express para servir archivos estáticos desde la carpeta 'public'
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta de ejemplo para renderizar el archivo index.ejs
+// Rutas
 app.use('/', require('./routes/home'));
 app.use('/index', require('./routes/home'));
+app.get('/chat', (req, res) => { res.render('chat') });
+app.get('/avisos', (req, res) => { res.render('avisos') });
+app.get('/cursos', (req, res) => { res.render('cursos') });
+app.get('/contacto', (req, res) => { res.render('contacto'); });
 
 
-// Otras rutas
-app.get('/avisos', (req, res) => {
-  res.render('avisos', { title: 'Noticias' });
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
-
-app.get('/cursos', (req, res) => {
-  res.render('cursos', { title: 'Cursos' });
-});
-
-app.get('/contacto', (req, res) => {
-  res.render('contacto', { title: 'Contacto' });
-});
-
-// Iniciar el servidor
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
