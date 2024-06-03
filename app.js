53db4e662649822dbcb59c4e8ef8739852823220
Borja <luisbirja556@gmail.com>
2024-06-03 09:40:25 -0600

    cambios borja

--- app.js
+++ app.js
@@ -50,6 +50,18 @@ app.get('/contacto', (req, res) => {
     res.render('contacto', { user });
 });
 
+// Manejar errores 404
+app.use((req, res, next) => {
+    res.status(404).render('404', { title: 'Página no encontrada' });
+  });
+  
+  // Manejar errores 500
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).render('500', { title: 'Error interno del servidor' });
+  });
+  
+
 // Iniciar el servidor
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
